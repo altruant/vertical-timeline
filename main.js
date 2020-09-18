816d600b53b9c19667549e1cd0998a83fa8130fe
altruant <noellehamberis@gmail.com>
2020-09-18 13:13:50 -0400

    full functionality

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-const = visElements = document.querySelectorAll('.timeline-item');
+
 
 const options = {
   root: null,
-  rootMargin: 20%;
-  threshold: .8;
+  rootMargin: '0%',
+  threshold: .8,
 }
+
+const intersectionObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting && entry.target.classList.contains('slide-right')) {
+      entry.target.classList.remove('fadeOut');
+      entry.target.classList.add('fadeRight');
+    } else if (entry.isIntersecting && entry.target.classList.contains('slide-left')) {
+      entry.target.classList.remove('fadeOut');
+      entry.target.classList.add('fadeLeft');
+    } else {
+      entry.target.classList.remove('fadeRight', 'fadeLeft');
+      entry.target.classList.add('fadeOut');
+    }
+  });
+}, options);
+
+// get your elements, by class name '.js-item'
+const timelineItems = [...document.querySelectorAll('.timeline-item')];
+// start observing your elements
+timelineItems.forEach((element) => intersectionObserver.observe(element));
